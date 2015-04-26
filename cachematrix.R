63b635e07a93bb6aeb69af03a005360e2bf86e32
Hanna <hannahourani@gmail.com>
2015-04-26 11:50:30 +0200

    Version two of RPROG assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions have been created for RPROG course. One is a function that sets
+## and gets matrix value then sets and gets inverse value. Second function caches the
+## the inverse of the matrix if it has been solved before, and calculates a new inverse
+##and caches if the matrix has not been solved before.
 
-## Write a short comment describing this function
+## This function is similar to the makeVector function of the example.
+##It is responsible for setting and getting the matrix value and set and getting
+##the inverse of the matrix value
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ 
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  
+  
+  list(set = set, get = get,
+      setinverse = setinverse,
+      getinverse = getinverse)
+  
 }
 
 
-## Write a short comment describing this function
+## caches the the inverse of the matrix if it has been solved before, 
+##and calculates a new inverse
+##and caches if the matrix has not been solved before
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+  print(class(x))
+  m <- x$getinverse()
+  
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setinverse(m)
+  m
 }
+
